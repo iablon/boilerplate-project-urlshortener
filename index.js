a7d1edd55964a255eb23c68bf5e2dc5b8d99fd3d
iablon <mysotto8@gmail.com>
2023-10-25 17:11:03 +0200

    url checking done

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
+const dns = require('dns');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+let ShortUrl = require('./ShortUrl');
+
+//provaInserimento();
+async function provaInserimento(){
+  try {
+    await new ShortUrl({
+      url: "https://hass.ss.pro"
+    }).save()
+    console.log('success');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI).catch((err)=>{console.log(err);})
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +38,18 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl',function(req,res){
+  
+  dns.lookup(req.body['url'].slice(8),(err,addr)=>{
+    if (err){
+      res.json({error: 'invalid url'});
+      return console.log(err);
+    }
+    else res.json({original_url: req.body['url'],short_url: 1})
+  })
+  
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
